a6bcd123bcc678b3b371f3d90b6f7caf4d972bd5
shenwei <shenwei1234ab@126.com>
2015-05-07 10:24:02 +0800

    FirstCommit
    The whole Game  project of Unity

---
+++ Assets/Editor/PathNodeDebug.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+public class PathNodeDebug : ScriptableObject {
+    public bool m_ifShowAllNode = false;
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+
+    [MenuItem("Debug/Show All PathNode")]
+    static void ShowAll()
+    {
+        GameObject nodeParent = GameObject.Find("PathNodeManager");
+        if(nodeParent)
+        {
+            foreach (Transform child in nodeParent.transform)
+            {
+                MeshRenderer render = child.GetComponent<MeshRenderer>();
+                render.enabled = true;
+             }  
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
