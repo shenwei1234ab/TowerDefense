b4586b94a32bfa8578ab284f581252667d34593c
shenwei <shenwei1234ab@126.com>
2015-05-25 21:27:39 +0800

    ui

---
+++ Assets/Script/UINormalButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+public enum ButtonType
+{
+    //CreateTower,
+    UpdateTower,
+    DestoryTower,
+    Exit,
+    NextScene,
+    BackToMain,
+    PauseGame,
+    ResumeGame,
+}
+public class UINormalButton : UiButton 
+{
+
+    public ButtonType m_buttonType;
+	// Use this for initialization
+	void Start () 
+    {
+        InputSystem.Instance().RegistUIEvent(this);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
---
+++ Assets/Script/UITowerButton.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UITowerButton : UiButton
+{
+    public GameObject m_anchorObj;
+    //对应tower的类型 
+     TowerType m_towerType;
+    // Use this for initialization
+
+
+    void Awake()
+     {
+         
+     }
+    void Start()
+    {
+        //像inputSystem注册事件
+        InputSystem.Instance().RegistUIEvent(this);
+   
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //设置uitowerbutton的类型并设置相应的sprite
+    public TowerType TowerType
+    {
+        get
+        {
+            return m_towerType;
+        }
+
+        set
+        {
+            //设置towertype并且设置sprite
+            m_towerType = value;
+           gameObject.GetComponent<UISprite>().spriteName = m_towerType.ToString() + "1";
+        }
+    }
+
+
+}
