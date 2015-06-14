ed954365ac6020d704c9266d8e40cfca15d3cc00
David Shen <shenwei1234ab@126.com>
2015-06-14 23:54:14 +0800

    SingleTon
    泛型单例

---
+++ Assets/Script/SingleTon/CSingleTon.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+//泛型单例
+public class CSingleTon <T>:MonoBehaviour where T:new() 
+{
+    class SingleTonCreator
+    {
+        static SingleTonCreator()
+        {
+
+        }
+        internal static readonly T m_instance = new T();  
+    }
+  
+    public static T Instance
+    {
+        get
+        {
+            return SingleTonCreator.m_instance;
+        }
+    }
+
+    void Awake()
+    {
+
+    }
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Script/SingleTon/SingleTonTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SingleTonTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+        SingleTon1.Instance.test();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
