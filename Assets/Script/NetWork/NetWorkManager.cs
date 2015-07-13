192889fe622db95f1e5ebf01a6935ad489bd45d6
David Shen <shenwei1234ab@126.com>
2015-07-14 00:02:00 +0800

    http timeout fix

--- Assets/Script/NetWork/NetWorkManager.cs
+++ Assets/Script/NetWork/NetWorkManager.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 
 public class NetWorkManager  :MonoBehaviour
 {
+  
     void Awake()
     {
         m_instance = this;
@@ -27,24 +28,22 @@ public class NetWorkManager  :MonoBehaviour
         byte[] bs = System.Text.UTF8Encoding.UTF8.GetBytes(postData);
 
         WWW www = new WWW(strUrl, bs, headers);
-  
         yield return www;
-
         if (www.error != null)
         {
            // m_info = www.error;
-            yield return null;
+            Debug.Log("Connect Error");
+            //resObj.StateCode = 1003;
+           // yield return null;
         }
-        //反序列化返回的json字符
-        resObj = (T)Protocal.JsonToObject<T>(www.text);
-
-        getDataFun(resObj);
-//            Debug.Log("返回的数据是 " + www.text);
-        //转化成对象
-       // JsonData obj = JsonMapper.ToObject(m_info);
-       // Debug.Log(obj["name"]);
-       // Debug.Log(obj["password"]);
-      //  Debug.Log(m_info);
+        else
+        {
+           // Debug.Log(i);
+             //反序列化返回的json字符
+              resObj = (T)Protocal.JsonToObject<T>(www.text);
+              getDataFun(resObj);
+        }
+        
     }
     StateCode m_StateCode;
 
@@ -54,13 +53,19 @@ public class NetWorkManager  :MonoBehaviour
 
     public void Login(Protocal.HTTP_RequestLogin reqLogin, Protocal.HTTP_ResponseLogin resLogin, GetDateHandle getDataFun)
     {
-        Debug.Log("Login");
+        //Debug.Log("Login");
         StartCoroutine(IPostData(reqLogin, "127.0.0.1:3000/login", resLogin, getDataFun));
+      
     }
 
 
     public void Register(Protocal.HTTP_RequestRegister reqRegister,Protocal.HTTP_ResponseRegister resRegister,GetDateHandle getDataFun)
     {
         StartCoroutine(IPostData(reqRegister, "127.0.0.1:3000/register", resRegister, getDataFun));
+    }
+
+    void Update()
+    {
+       
     }
 }
